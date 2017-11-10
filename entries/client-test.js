7a710dcf80928a2d3f567175be8a4efcfbe6bf9b
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-11-10 11:19:56 -0800

    Remove vestigial test entries

--- entries/client-test.js
+++
@@ -1,5 +0,0 @@
-/* eslint-env node */
-
-require('__BROWSER_TEST_ENTRY__');
-// const context = require.context('__BROWSER_TEST_ENTRY__', true);
-// context.keys().map(context);
--- entries/server-test.js
+++
@@ -1,5 +0,0 @@
-/* eslint-env node */
-require('__NODE_TEST_ENTRY__');
-
-// const context = require.context('__NODE_TEST_ENTRY__', true);
-// context.keys().map(context);
