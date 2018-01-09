cc1b5b22a85ddaee5e84f32ea16927dc77abf1bc
Kevin Grandon <kevingrandon@yahoo.com>
2018-01-09 10:28:35 -0800

    Add rejection timeout after several minutes - will most likely not be used.

--- build/merge-coverage.js
+++ build/merge-coverage.js
@@ -4,17 +4,23 @@ const fs = require('fs');
 // Jest writes coverage after the process exits
 // so we need to poll and wait for all coverage files.
 const waitForAllCoverage = config => {
+  const maxTries = 10000;
+  let numTries = 0;
   return Promise.all(
     config.environments.map(
       env =>
-        new Promise(resolve => {
+        new Promise((resolve, reject) => {
           const interval = setInterval(() => {
             const coverageExists = fs.existsSync(
               `${config.dir}/coverage-${env}/coverage-final.json`
             );
+            numTries += 1;
             if (coverageExists) {
-              resolve();
               clearInterval(interval);
+              resolve();
+            } else if (numTries > maxTries) {
+              clearInterval(interval);
+              reject();
             }
           }, 100);
         })
