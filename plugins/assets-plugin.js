be481c9d29f1f727b32e05c0a1e562463163453d
Giancarlo Anemone <ganemone@uber.com>
2018-01-12 09:24:39 -0800

    Fix flow

--- plugins/assets-plugin.js
+++ plugins/assets-plugin.js
@@ -6,7 +6,7 @@ const path = require('path');
 const mount = require('koa-mount');
 const serve = require('koa-static');
 
-module.exports = function(dir) {
+module.exports = function(dir /* : string */) {
   const {assetPath, env} = envVarsPlugin().of();
   // setting defer here tells the `serve` middleware to `await next` first before
   // setting the response. This allows composition with user middleware
