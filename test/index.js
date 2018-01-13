3fb113fb275f785dc08aa8021d0db089b0777555
Kevin Grandon <kevingrandon@yahoo.com>
2018-01-12 17:05:39 -0800

    Implement test parallelism

--- test/index.js
+++ test/index.js
@@ -1,19 +1,35 @@
 /* eslint-env node */
-require('./cli/dev');
-require('./cli/test-app');
-require('./cli/test');
-require('./cli/build');
-require('./compiler/api');
-require('./compiler/errors');
 
-/*
-require('./browser-support');
-*/
-require('../build/babel-plugins/babel-plugin-asseturl/test');
-require('../build/babel-plugins/babel-plugin-chunkid/test');
-require('../build/babel-plugins/babel-plugin-experimentation/test');
-require('../build/babel-plugins/babel-plugin-i18n/test');
-require('../build/babel-plugins/babel-plugin-sw/test');
-require('../build/babel-plugins/babel-plugin-sync-chunk-ids/test');
-require('../build/babel-plugins/babel-plugin-sync-chunk-paths/test');
-require('../build/babel-plugins/babel-plugin-utils/test');
+const testFiles = [
+  './cli/dev',
+  './cli/test-app',
+  './cli/test',
+  './cli/build',
+  './compiler/api',
+  './compiler/errors',
+
+  /*
+  './browser-support',
+  */
+  '../build/babel-plugins/babel-plugin-asseturl/test',
+  '../build/babel-plugins/babel-plugin-chunkid/test',
+  '../build/babel-plugins/babel-plugin-experimentation/test',
+  '../build/babel-plugins/babel-plugin-i18n/test',
+  '../build/babel-plugins/babel-plugin-sw/test',
+  '../build/babel-plugins/babel-plugin-sync-chunk-ids/test',
+  '../build/babel-plugins/babel-plugin-sync-chunk-paths/test',
+  '../build/babel-plugins/babel-plugin-utils/test',
+];
+
+const thisChunk = process.env.THIS_CHUNK
+  ? parseInt(process.env.THIS_CHUNK, 10)
+  : 0;
+const totalChunks = process.env.TOTAL_CHUNKS
+  ? parseInt(process.env.TOTAL_CHUNKS, 10)
+  : 1;
+
+testFiles.forEach((file, index) => {
+  if (index % totalChunks === thisChunk) {
+    require(file);
+  }
+});
