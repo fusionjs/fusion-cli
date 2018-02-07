5bcbffa2892704e4e823705149759542656f2c85
Giancarlo Anemone <ganemone@uber.com>
2018-02-07 12:38:43 -0800

    Implement shared config and turn off caching

--- build/node/jest.config.js
+++ build/node/jest.config.js
@@ -1,24 +1,11 @@
 /* eslint-env node */
 
-const testFolder = process.env.TEST_FOLDER || '__tests__';
+const baseJestConfig = require('../base-jest-config.js');
 
 module.exports = {
-  cache: true,
-  coverageDirectory: `${process.cwd()}/coverage`,
-  coverageReporters: ['json'],
+  ...baseJestConfig,
   displayName: 'node',
   browser: false,
-  rootDir: process.cwd(),
   testEnvironment: 'node',
   testPathIgnorePatterns: ['.*\\.browser\\.js'],
-  transform: {
-    '^.+\\.js$': require.resolve('../jest-transformer.js'),
-  },
-  transformIgnorePatterns: ['/node_modules/(?!(fusion-cli.*build))'],
-  setupFiles: [
-    require.resolve('../jest-framework-shims.js'),
-    require.resolve('../jest-framework-setup.js'),
-  ],
-  snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
-  testMatch: [`**/${testFolder}/**/*.js`],
 };
