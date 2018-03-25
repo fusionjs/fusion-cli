7d9152456e8d85981f08c65cc59f82d67aaba698
Kevin Grandon <kevingrandon@yahoo.com>
2018-03-25 13:22:03 +0900

    Add test case with failing production build

--- test/fixtures/dynamic-import-app/src/main.js
+++ test/fixtures/dynamic-import-app/src/main.js
@@ -1,9 +1,15 @@
 // @flow
 import React from 'react';
 import App from 'fusion-react';
-import root from './root.js';
+import Router from 'fusion-plugin-react-router';
+import UniversalEvents, {
+  UniversalEventsToken,
+} from 'fusion-plugin-universal-events';
+import Root from './root.js';
 
 export default async function start() {
-  const app = new App(root);
+  const app = new App(<Root />);
+  app.register(Router, {});
+  app.register(UniversalEventsToken, UniversalEvents);
   return app;
 }
