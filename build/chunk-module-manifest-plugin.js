b569928f7fe608d30e863bb3965a092f73f1f0af
Kevin Grandon <kevingrandon@yahoo.com>
2018-03-26 13:54:42 +0900

    Iterate over rootModules.resources to add all files to manifest

--- build/chunk-module-manifest-plugin.js
+++ build/chunk-module-manifest-plugin.js
@@ -22,8 +22,18 @@ class ChunkModuleManifestPlugin {
           const chunkIdsByFile = new Map();
           chunks.forEach(c => {
             const chunkId = c.id;
-            const files = Array.from(c.modulesIterable, m => m.resource);
-            files.forEach(path => {
+
+            let additionalFileDeps = [];
+            const files = Array.from(c.modulesIterable, m => {
+              if (m.rootModule && m.rootModule.resource) {
+                additionalFileDeps = additionalFileDeps.concat(
+                  Array.from(m.buildInfo.fileDependencies)
+                );
+              }
+              return m.resource || (m.rootModule && m.rootModule.resource);
+            });
+            const allFiles = files.concat(additionalFileDeps);
+            allFiles.forEach(path => {
               if (!chunkIdsByFile.has(path)) {
                 chunkIdsByFile.set(path, new Set());
               }
