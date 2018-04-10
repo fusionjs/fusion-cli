a46d09bb973690d8b07cfc582a0af1eaac0c2205
Kevin Grandon <kevingrandon@yahoo.com>
2018-04-10 13:10:12 -0700

    Update comment

--- test/cli/dev.js
+++ test/cli/dev.js
@@ -196,7 +196,8 @@ test('`fusion dev` recovering from errors', async t => {
     );
     next();
   });
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  // Need a wait here before saving otherwise the watcher won't pick up the edited file.
+  await new Promise(resolve => setTimeout(resolve, 500));
   fs.writeFileSync(mainPath, fs.readFileSync(mainPath));
 });
 
