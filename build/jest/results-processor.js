443e2c03beb88679408a387ba5a58ce98c6cffce
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-04-23 14:57:15 -0700

    Only write test results in CI

--- build/jest/results-processor.js
+++ build/jest/results-processor.js
@@ -1,17 +1,20 @@
 /* eslint-env node */
 
 const fs = require('fs');
+const ci = require('ci-info');
 
 module.exports = function resultsProcessor(results) {
-  fs.mkdir('.fusion', err => {
-    if (err && err.code !== 'EEXIST') throw err;
-    fs.writeFile(
-      './.fusion/test-results.json',
-      JSON.stringify(results, null, 2),
-      err => {
-        if (err) throw err;
-      }
-    );
-  });
+  if (ci.isCI) {
+    fs.mkdir('.fusion', err => {
+      if (err && err.code !== 'EEXIST') throw err;
+      fs.writeFile(
+        './.fusion/test-results.json',
+        JSON.stringify(results, null, 2),
+        err => {
+          if (err) throw err;
+        }
+      );
+    });
+  }
   return results;
 };
