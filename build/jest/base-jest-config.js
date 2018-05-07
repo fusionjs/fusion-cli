c34fdbaa1cb8438d0d60c24e64b66a49008ee1eb
Leo Horie <lhorie@uber.com>
2018-05-07 11:26:44 -0700

    Add support for react 15 in tests

--- build/jest/base-jest-config.js
+++ build/jest/base-jest-config.js
@@ -2,6 +2,12 @@
 
 const testFolder = process.env.TEST_FOLDER || '__tests__';
 
+const meta = require(process.cwd() + '/package.json');
+const reactVersion = meta.dependencies.react
+  .split('.')
+  .shift()
+  .match(/\d+/);
+
 module.exports = {
   cache: false,
   coverageDirectory: `${process.cwd()}/coverage`,
@@ -13,7 +19,7 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!(fusion-cli.*build))'],
   setupFiles: [
     require.resolve('./jest-framework-shims.js'),
-    require.resolve('./jest-framework-setup.js'),
+    require.resolve(`./jest-framework-setup-${reactVersion}.js`),
   ],
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
   testMatch: [`**/${testFolder}/**/*.js`],
