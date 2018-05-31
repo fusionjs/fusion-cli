fusionjs/fusion-cli

commit 5253325dd529e2cd154f950b6a84883c6a004111

Ensure coverage reports include .js files that aren't imported in tests
https://github.com/fusionjs/fusion-cli/pull/336

build/jest/base-jest-config.js
@@ -17,4 +17,5 @@ module.exports = {
   ],
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
   testMatch: [`**/${testFolder}/**/*.js`],
+  collectCoverageFrom: ['**.js', '!**/__integration__/**'],
 };

build/jest/jest-config.js
@@ -1,10 +1,15 @@
 /* eslint-env node */
 
+const baseJestConfig = require('./base-jest-config.js');
+
 const projects = process.env.JEST_ENV.split(',');
 
 let config = {
   cache: false,
   coverageDirectory: `${process.cwd()}/coverage`,
+  // collectCoverageFrom doesn't work from project config,
+  // must be set at top-level
+  collectCoverageFrom: baseJestConfig.collectCoverageFrom,
 };
 
 // Use projects if we have more than one environment.

test/cli/test-app.js
@@ -1,338 +1,0 @@
-/* eslint-env node */
-
-const fs = require('fs');
-const path = require('path');
-const test = require('tape');
-
-const CDP = require('chrome-remote-interface');
-const {promisify} = require('util');
-const exec = promisify(require('child_process').exec);
-const spawn = require('child_process').spawn;
-
-const readFile = promisify(fs.readFile);
-
-const countTests = require('../fixtures/test-jest-app/count-tests');
-
-const runnerPath = require.resolve('../../bin/cli-runner');
-
-test('`fusion test-app` passes', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=passes`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 2, 'ran 2 tests');
-  t.end();
-});
-
-test('`fusion test-app` failure', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=fails`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  try {
-    await exec(`node -e "${cmd}"`);
-    t.fail('should not succeed');
-  } catch (e) {
-    t.equal(countTests(e.message), 2, 'ran 2 tests');
-    t.notEqual(e.code, 0, 'exits with non-zero status code');
-    t.end();
-  }
-});
-
-test('`fusion test-app` all passing tests', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=pass`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 4, 'ran 4 tests');
-  t.end();
-});
-
-test('`fusion test-app` expected test passes in browser/node', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=pass-`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 2, 'ran 2 tests');
-
-  t.end();
-});
-
-test('`fusion test-app` expected tests fail when run in browser/node', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=fail-`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  try {
-    await exec(`node -e "${cmd}"`);
-    t.fail('should not succeed');
-  } catch (e) {
-    t.notEqual(e.code, 0, 'exits with non-zero status code');
-    t.equal(countTests(e.message), 2, 'ran 2 tests');
-    t.end();
-  }
-});
-
-test('`fusion test-app --testFolder=integration` runs correct tests', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --env=node --testFolder=__integration__`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 1, 'ran 1 test');
-
-  t.end();
-});
-
-test('`fusion test-app` snapshotting', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=snapshot-no-match`;
-
-  const snapshotFile =
-    __dirname +
-    '/../fixtures/test-jest-app/__tests__/__snapshots__/snapshot-no-match.js.fixture';
-  const backupSnapshot =
-    __dirname + '/../fixtures/snapshots/snapshot-no-match.js.fixture';
-
-  // Copy fixture to snapshot
-  fs
-    .createReadStream(snapshotFile)
-    .pipe(fs.createWriteStream(snapshotFile.replace(/fixture$/, 'snap')));
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  try {
-    await exec(`node -e "${cmd}"`);
-    t.fail('should not succeed');
-  } catch (e) {
-    t.notEqual(e.code, 0, 'exits with non-zero status code');
-    t.equal(countTests(e.message), 2, 'ran 2 tests');
-  }
-
-  const updateSnapshot = `require('${runnerPath}').run('${args} --updateSnapshot')`;
-  await exec(`node -e "${updateSnapshot}"`);
-
-  const newSnapshotCode = await readFile(snapshotFile);
-  const originalSnapshotCode = await readFile(backupSnapshot);
-  t.notEqual(newSnapshotCode, originalSnapshotCode, 'snapshot is updated');
-
-  fs.unlinkSync(snapshotFile.replace(/fixture$/, 'snap'));
-
-  t.end();
-});
-
-test('`fusion test-app` snapshotting with -u option', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=snapshot-no-match`;
-
-  const snapshotFile =
-    __dirname +
-    '/../fixtures/test-jest-app/__tests__/__snapshots__/snapshot-no-match.js.fixture';
-  const backupSnapshot =
-    __dirname + '/../fixtures/snapshots/snapshot-no-match.js.fixture';
-
-  // Copy fixture to snapshot
-  fs
-    .createReadStream(snapshotFile)
-    .pipe(fs.createWriteStream(snapshotFile.replace(/fixture$/, 'snap')));
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  try {
-    await exec(`node -e "${cmd}"`);
-    t.fail('should not succeed');
-  } catch (e) {
-    t.notEqual(e.code, 0, 'exits with non-zero status code');
-    t.equal(countTests(e.message), 2, 'ran 2 tests');
-  }
-
-  const updateSnapshot = `require('${runnerPath}').run('${args} -u')`;
-  await exec(`node -e "${updateSnapshot}"`);
-
-  const newSnapshotCode = await readFile(snapshotFile);
-  const originalSnapshotCode = await readFile(backupSnapshot);
-  t.notEqual(newSnapshotCode, originalSnapshotCode, 'snapshot is updated');
-
-  fs.unlinkSync(snapshotFile.replace(/fixture$/, 'snap'));
-
-  t.end();
-});
-
-test('`fusion test-app` snapshotting - enzyme serializer', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=snapshot-enzyme-no-match`;
-
-  const snapshotFile =
-    __dirname +
-    '/../fixtures/test-jest-app/__tests__/__snapshots__/snapshot-enzyme-no-match.js.fixture';
-  const backupSnapshot =
-    __dirname + '/../fixtures/snapshots/snapshot-enzyme-no-match.js.fixture';
-
-  // Copy fixture to snapshot
-  fs
-    .createReadStream(snapshotFile)
-    .pipe(fs.createWriteStream(snapshotFile.replace(/fixture$/, 'snap')));
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  try {
-    await exec(`node -e "${cmd}"`);
-    t.fail('should not succeed');
-  } catch (e) {
-    t.notEqual(e.code, 0, 'exits with non-zero status code');
-    t.equal(countTests(e.message), 2, 'ran 2 tests');
-  }
-
-  const updateSnapshot = `require('${runnerPath}').run('${args} --updateSnapshot')`;
-  await exec(`node -e "${updateSnapshot}"`);
-
-  const newSnapshotCode = await readFile(snapshotFile);
-  const originalSnapshotCode = await readFile(backupSnapshot);
-  t.notEqual(newSnapshotCode, originalSnapshotCode, 'snapshot is updated');
-
-  fs.unlinkSync(snapshotFile.replace(/fixture$/, 'snap'));
-
-  t.end();
-});
-
-test('`fusion test-app` dynamic imports', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=dynamic-imports`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 2, 'ran 2 tests');
-  t.end();
-});
-
-test('`fusion test-app` coverage', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --coverage --match=passes`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 2, 'ran 2 tests');
-
-  // Look for something like coverage
-  t.ok(response.stdout.includes('Uncovered Lines'));
-  t.end();
-});
-
-test('`fusion test-app` cobertura coverage reports', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --coverage --match=passes`;
-
-  const cmd = `require('${runnerPath}').run('${args} --env=jsdom')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 1, 'ran 1 tests');
-
-  const cobertunaReport = await readFile(
-    path.resolve(dir, 'coverage/cobertura-coverage.xml')
-  );
-  // Only a single report should be generated
-  t.ok(cobertunaReport.includes('<line number="1" hits="1"/>'));
-  t.ok(!cobertunaReport.includes('<line number="1" hits="2"/>'));
-
-  // Assert that there's two hits when combining coverage
-  const cmd2 = `require('${runnerPath}').run('${args}')`;
-  const response2 = await exec(`node -e "${cmd2}"`);
-  t.equal(countTests(response2.stderr), 2, 'ran 2 tests');
-
-  const combinedReport = await readFile(
-    path.resolve(dir, 'coverage/cobertura-coverage.xml')
-  );
-
-  t.ok(combinedReport.includes('<line number="1" hits="2"/>'));
-  t.ok(!combinedReport.includes('<line number="1" hits="1"/>'));
-  t.end();
-});
-
-test('`fusion test-app` environment variables', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --match=environment-variables`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`, {
-    env: Object.assign({}, process.env, {
-      NODE_ENV: 'development',
-    }),
-  });
-  t.equal(countTests(response.stderr), 2, 'ran 2 tests');
-
-  t.end();
-});
-
-test('`fusion test-app` uses .fusionjs.js', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-babel');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const response = await exec(`node -e "${cmd}"`);
-  t.equal(countTests(response.stderr), 2, 'ran 2 tests');
-
-  t.end();
-});
-
-async function triggerCodeStep() {
-  return new Promise(resolve => {
-    CDP({port: '9229'}, async client => {
-      const {Runtime} = client;
-      await Runtime.runIfWaitingForDebugger();
-      await client.close();
-      resolve();
-    }).on('error', err => {
-      throw err;
-    });
-  });
-}
-
-test('`fusion test-app --debug --env=jsdom,node`', async t => {
-  const dir = path.resolve(__dirname, '../fixtures/test-jest-app');
-  const args = `test-app --dir=${dir} --configPath=../../../build/jest/jest-config.js --debug --env=jsdom,node  --match=passes`;
-
-  const cmd = `require('${runnerPath}').run('${args}')`;
-  const stderrLines = [];
-  const child = spawn('node', ['-e', cmd]);
-
-  const listenAddresses = {};
-  let numResults = 0;
-  child.stderr &&
-    child.stderr.on('data', data => {
-      const line = data.toString();
-      // eslint-disable-next-line no-console
-      console.log(` - received spawn line: ${line}`);
-      stderrLines.push(line);
-      // Keep track of all addresses that we start listening to.
-      if (line.startsWith('Debugger listening on ws')) {
-        listenAddresses[line] = true;
-      }
-      // Wait until we have results for both environments before ending the test.
-      if (/Tests:.*2\s+passed,\s+2\s+total/.test(line)) {
-        numResults += 1;
-        if (numResults == 1) {
-          t.end();
-        }
-      }
-    });
-
-  // Poll until we get a listener message.
-  async function checkStartedMessageCount(count) {
-    return new Promise(async resolve => {
-      while (Object.keys(listenAddresses).length < count) {
-        await new Promise(r => setTimeout(r, 100));
-      }
-      resolve();
-    });
-  }
-
-  // Step through the environment
-  await checkStartedMessageCount(1);
-  await triggerCodeStep();
-
-  t.ok(
-    Object.keys(listenAddresses).length >= 1,
-    'found a remote debug connection'
-  );
-
-  child.kill();
-});

test/cli/test.js
@@ -233,6 +233,11 @@ test('`fusion test` cobertura coverage reports', async t => {
   t.ok(cobertunaReport.includes('<line number="1" hits="1"/>'));
   t.ok(!cobertunaReport.includes('<line number="1" hits="2"/>'));
 
+  t.ok(
+    cobertunaReport.includes('not-imported-in-tests.js'),
+    'report includes files not imported in tests'
+  );
+
   // Assert that there's two hits when combining coverage
   const cmd2 = `require('${runnerPath}').run('${args}')`;
   const response2 = await exec(`node -e "${cmd2}"`);
@@ -244,6 +249,11 @@ test('`fusion test` cobertura coverage reports', async t => {
 
   t.ok(combinedReport.includes('<line number="1" hits="2"/>'));
   t.ok(!combinedReport.includes('<line number="1" hits="1"/>'));
+
+  t.ok(
+    combinedReport.includes('not-imported-in-tests.js'),
+    'report includes files not imported in tests'
+  );
   t.end();
 });
 

test/fixtures/test-jest-app/foo.js
@@ -1,0 +1,7 @@
+export function foo() {
+  return true;
+}
+
+export function unused() {
+  return false;
+}

test/fixtures/test-jest-app/main.js
@@ -1,3 +1,5 @@
+import {foo} from './foo.js';
+
 export default function () {
-  return true;
+  return foo();
 }

test/fixtures/test-jest-app/not-imported-in-tests.js
@@ -1,0 +1,3 @@
+export function notImportedInTests() {
+  return true
+}

commit 11abf7cce6f73f1938d3a2f6be7f21af3bbcd0e3

Fix coverage inclusion pattern
This broke in 5253325dd529e2cd154f950b6a84883c6a004111 because the pattern does not recursively match js files.

build/jest/base-jest-config.js
@@ -38,6 +38,10 @@ module.exports = {
   ],
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
   testMatch: [`**/${testFolder}/**/*.js`],
-  collectCoverageFrom: ['**.js', '!**/__integration__/**'],
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/__integration__/**',
+    '!**/node_modules/**'
+  ],
   testResultsProcessor: require.resolve('./results-processor.js'),
 };