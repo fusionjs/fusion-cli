1cedccb4c474af1bf2d21c4afa68f50c2409aea9
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-06-14 16:26:18 -0700

    Fix typo

--- build/babel-plugins/babel-plugin-transform-tree-shake/index.js
+++ build/babel-plugins/babel-plugin-transform-tree-shake/index.js
@@ -28,7 +28,7 @@ module.exports = (
 
           const localPath = specifier.get('local');
           const localName = localPath.node.name;
-          // This should not be hardoded to React and/or improve compat with JSX transform
+          // This should not be hardcoded to React and/or improve compat with JSX transform
           // However, this stopgap is fine until this plugin is made redundant by
           // https://github.com/webpack/webpack/issues/7519
           if (localName === 'React') {
