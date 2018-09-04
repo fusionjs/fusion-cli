395d1c2b08371ace48b7b2a45ce1cc4d09dee923
Kevin Grandon <kevingrandon@yahoo.com>
2018-09-04 10:53:44 -0700

    Support a whitelist of Jest CLI arguments

--- commands/test.js
+++ commands/test.js
@@ -13,30 +13,66 @@ const {TestAppRuntime} = require('../build/test-runtime');
 exports.run = async function(
   {
     dir = '.',
-    watch,
     debug,
     match,
     env,
     testFolder,
-    updateSnapshot,
-    coverage,
     configPath,
     // Allow snapshots to be updated using `-u` as well as --updateSnapshot.
     // We don't document this argument, but since jest output automatically
     // suggests this as a valid argument, we support it in case it's used.
     u,
+    updateSnapshot,
+    // Arguments which are passed through into jest
+    ...rest
   } /*: any */
 ) {
+  const jestArgs /*: any */ = {
+    updateSnapshot: updateSnapshot || u || false,
+  };
+  const whitelistedJestOptions = [
+    'changedFilesWithAncestor',
+    'changedSince',
+    'ci',
+    'clearCache',
+    'colors',
+    'coverage',
+    'detectOpenHandles',
+    'errorOnDeprecated',
+    'expand',
+    'findRelatedTests',
+    'json',
+    'lastCommit',
+    'listTests',
+    'logHeapUsage',
+    'noStackTrace',
+    'notify',
+    'onlyChanged',
+    'passWithNoTests',
+    'reporters',
+    'showConfig',
+    'silent',
+    'testLocationInResults',
+    'useStderr',
+    'version',
+    'watch',
+    'watchAll',
+    'watchman',
+  ];
+  whitelistedJestOptions.forEach(arg => {
+    if (rest[arg]) {
+      jestArgs[arg] = rest[arg];
+    }
+  });
+
   const testRuntime = new TestAppRuntime({
     dir,
-    watch,
     debug,
     match,
     env,
     testFolder,
-    updateSnapshot: updateSnapshot || u,
-    coverage,
     configPath,
+    jestArgs,
   });
 
   // $FlowFixMe
