fa264ef89b7097a2204a47ab4c0ab864bc20c47f
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-12-10 22:26:44 -0800

    Remove flow-typed todo

--- flow-typed/npm/jest_v23.x.x.js
+++ flow-typed/npm/jest_v23.x.x.js
@@ -1141,7 +1141,6 @@ declare var expect: {
   },
 };
 
-// TODO handle return type
 // http://jasmine.github.io/2.4/introduction.html#section-Spies
 declare function spyOn(value: mixed, method: string): Object;
 
