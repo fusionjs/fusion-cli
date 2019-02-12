29c31db4ed378a5396859c311ffa1d5223cdc5b4
Leo Horie <lhorie@uber.com>
2019-02-12 15:06:33 -0800

    add other integration test

--- test/e2e/i18n/test.js
+++ test/e2e/i18n/test.js
@@ -11,8 +11,56 @@
 const path = require('path');
 const puppeteer = require('puppeteer');
 
-const {test} = require('fusion-test-utils');
+const {createPlugin} = require('fusion-core');
+const {default: App} = require('fusion-react');
+const React = require('react');
+const {test, getSimulator} = require('fusion-test-utils');
 const {dev} = require('../utils.js');
+
+const {
+  default: Plugin,
+  I18nToken,
+  I18nLoaderToken,
+  Translate,
+  withTranslations,
+} = require('fusion-plugin-i18n-react');
+
+test('plugin', async t => {
+  const data = {test: 'hello', interpolated: 'hi ${value}'};
+  function Test(props) {
+    t.equal(typeof props.translate, 'function');
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(Translate, {id: 'test'})
+    );
+  }
+  const Root = withTranslations(['test'])(Test);
+  const app = new App(React.createElement(Root));
+  app.register(I18nToken, Plugin);
+  app.register(
+    I18nLoaderToken,
+    createPlugin({
+      provides() {
+        return {from: () => ({translations: data, locale: 'en_US'})};
+      },
+    })
+  );
+  app.middleware({i18n: I18nToken}, ({i18n}) => {
+    return (ctx, next) => {
+      const translator = i18n.from(ctx);
+      t.equal(translator.translate('test'), 'hello');
+      t.equal(
+        translator.translate('interpolated', {value: 'world'}),
+        'hi world'
+      );
+      return next();
+    };
+  });
+  const simulator = getSimulator(app);
+  const ctx = await simulator.render('/');
+  t.ok(typeof ctx.body === 'string' && ctx.body.includes('hello'));
+});
 
 test('able to do simple translations in React 16 - leverages React.Fragment', async t => {
   const dir = path.resolve(__dirname, 'fixture');
